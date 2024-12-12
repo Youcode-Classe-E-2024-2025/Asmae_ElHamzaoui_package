d145f2f0c3346fa3ebf64c82f49cd634d18f1298
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-12 13:06:01 +0100

    l'ajout d'une class formulaires

--- formulaires.php
+++ formulaires.php
@@ -19,7 +19,7 @@
 
 
     <!-- début  section1 -->
-      <section id="package">
+      <section id="package" class="formulaires">
        <!-- Formulaire pour insérer un nouvel package -->
        <form action="" method="post">
         <label for="">Nom du package : </label><br>
@@ -38,7 +38,7 @@
 
 
     <!-- début  section2 -->
-      <section id="auteur">
+      <section id="auteur" class="formulaires">
          <!-- Formulaire pour insérer un nouvel auteur -->
        <form action="" method="post">
         <label for="">Nom d'auteur : </label><br>
@@ -59,7 +59,7 @@
     <!-- Fin  section2 -->
 
     <!-- début  section3 -->
-      <section id="version">
+      <section id="version" class="formulaires">
 
       </section>
     <!-- Fin  section3 -->
