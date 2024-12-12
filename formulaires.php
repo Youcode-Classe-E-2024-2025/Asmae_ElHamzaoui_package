61709351986f21c602ab900f442b443b21160cb1
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-12 11:17:17 +0100

    création du fichier formulaires.php

---
+++ formulaires.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <!-- début  section1 -->
+      <section id="package">
+
+      </section>
+    <!-- Fin  section1 -->
+
+    <!-- début  section2 -->
+      <section id="auteur">
+
+      </section>
+    <!-- Fin  section2 -->
+
+    <!-- début  section3 -->
+      <section id="version">
+
+      </section>
+    <!-- Fin  section3 -->
+
+</body>
+
+</html>
