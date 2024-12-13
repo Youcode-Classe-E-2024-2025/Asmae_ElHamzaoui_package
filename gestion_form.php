207dc1de9c09df915e0319fc6e788d13fdb31022
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-13 07:20:44 +0100

    traitement pour ajouter un auteur

--- gestion_form.php
+++ gestion_form.php
@@ -20,3 +20,23 @@ include 'db.php';  // Connexion à la base de données
         }
         $stmt->close();
     }
+
+
+    // Formulaire pour ajouter un auteur
+    if (isset($_POST['nom_auteur'])) {
+        $nom_auteur = $_POST['nom_auteur'];
+        $prenom_auteur = $_POST['prenom_auteur'];
+        $email_auteur = $_POST['email_auteur'];
+        $date_inscription_auteur = $_POST['date_inscription_auteur'];
+
+        $sql = "INSERT INTO auteur (nom_Auteur, prenom_Auteur, email_Auteur, date_inscription_Auteur) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssss", $nom_auteur, $prenom_auteur, $email_auteur, $date_inscription_auteur);
+
+        if ($stmt->execute()) {
+            echo "Auteur ajouté avec succès!";
+        } else {
+            echo "Erreur : " . $stmt->error;
+        }
+        $stmt->close();
+    }
